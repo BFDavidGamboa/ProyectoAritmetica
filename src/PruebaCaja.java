76e17c9a4b756444e754a473c1d10a7abd957433
BFDavidGamboa <david.gamboa@blankfactor.com>
2023-08-12 13:24:47 -0500

    code cleaned and stuff

--- src/PruebaCaja.java
+++ src/PruebaCaja.java
@@ -2,21 +2,25 @@ import java.util.Scanner;
 public class PruebaCaja {
 
     public static void main(String[] args) {
-        Caja caja = new Caja();
+
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter height, width and large:");
 
-        System.out.println("Enter height: ");
-        caja.height = myObj.nextDouble();
 
-        System.out.println("Enter width: ");
-        caja.width = myObj.nextDouble();
+        System.out.println("Enter height in cm: ");
+        double height = myObj.nextDouble();
 
-        System.out.println("Enter large: ");
-        caja.large = myObj.nextDouble();
+        System.out.println("Enter width in cm: ");
+        double width = myObj.nextDouble();
+
+        System.out.println("Enter large in cm: ");
+        double large = myObj.nextDouble();
+
+        Caja caja = new Caja(large,width,height);
+
 
         double volume = caja.calculateVolume();
-        System.out.println("Se calculo el volumen de la caja " + volume);
+        System.out.println("Se calculo el volumen de la caja V= " + volume + " cm3");
 
     }
 
