2a168b08edceb370c9af5df37f8e03a0a0d5b35c
BFDavidGamboa <david.gamboa@blankfactor.com>
2023-08-12 11:32:41 -0500

    Basics operations

---
+++ src/PruebaAritmetica.java
@@ -0,0 +1,13 @@
+public class PruebaAritmetica {
+
+    public static void main(String[] args) {
+        Aritmetica aritmetica = new Aritmetica();
+
+
+        int result = aritmetica.sumar(1,2);
+        System.out.println("resutladoL " + result);
+
+    }
+
+}
+0
