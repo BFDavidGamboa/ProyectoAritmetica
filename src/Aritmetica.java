77b4a4f34fbc292e270201c09c1a86586db20f69
BFDavidGamboa <david.gamboa@blankfactor.com>
2023-08-12 12:53:33 -0500

    se crean nuevos metodos y se especifica el uso de variables de clase y variables de metodo y el uso de la palabra reservada this

--- src/Aritmetica.java
+++ src/Aritmetica.java
@@ -1,9 +1,38 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Aritmetica {
+
+    int a;
+    int b;
+    public Aritmetica(){
+
+        System.out.println("Ejecutando constructor vacio");
+    }
+
+    public Aritmetica(int arg1, int arg2) {
+
+        this.a = arg1;
+        this.b = arg2;
+    }
    public int sumar(int a, int b){
        int result = a + b;
        return result;
    }
+
+   public int sumarAtributosDeClase(){
+         return this.a +this.b;
+   }
+
+    public int restar(){
+        return this.a - this.b;
+    }
+
+    public int multiplicar(){
+        return this.a * this.b;
+    }
+
+    public int dividir(){
+        return this.a / this.b;
+    }
 
 }
--- src/PruebaAritmetica.java
+++ src/PruebaAritmetica.java
@@ -2,12 +2,48 @@ public class PruebaAritmetica {
 
     public static void main(String[] args) {
         Aritmetica aritmetica = new Aritmetica();
-
+        aritmetica.a = 10;
+        aritmetica.b = 3;
 
         int result = aritmetica.sumar(1,2);
-        System.out.println("resutladoL " + result);
+        int result2 = aritmetica.sumarAtributosDeClase();
+
+        System.out.println("Resutlado:  " + result);
+        System.out.println("Resutlado:  " + result2);
+
+
+        //Creation of second object aritmetica
+        Aritmetica aritmetica2 = new Aritmetica(4,2);
+
+        System.out.println("resultado 2 = " + aritmetica2.sumarAtributosDeClase());
+
+        int operandoA = 6;
+        int operandoB = 2;
+
+        //Creamos un objeto de la clase aritmetica enviando argumentos
+
+        Aritmetica objeto1 = new Aritmetica(operandoA, operandoB);
+
+        //imprimimos los valores de los operandos
+        System.out.println("operandoA = " + operandoA);
+        System.out.println("operandoB = " + operandoB);
+
+        //Imprimimoss el resultado de la suma
+
+        System.out.println("\nResultado suma: " + objeto1.sumarAtributosDeClase())  ;
+
+        //Imprimimos el resultado de la resta
+        System.out.println("\nResultado resta: " + objeto1.restar());
+
+        //Imprimimos el resultado de la multiplicacion
+        System.out.println("\nResultado de la multiplicacion: " + objeto1.multiplicar());
+
+
+        //Imprimimos el resultado de la division
+        System.out.println("\nResultado de la multiplicacion: " + objeto1.dividir());
 
     }
 
+
 }
-0
+
